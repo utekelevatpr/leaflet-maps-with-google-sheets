320c29745acbbb4bde8431b5e8c937cb942753e9
utekelevatpr <30903347+utekelevatpr@users.noreply.github.com>
2017-09-18 12:57:08 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -641,7 +641,16 @@ $(window).on('load', function() {
     }
 
     // Add Mapzen search control
-
+    if (getSetting('_mapSearch') !== 'off') {
+      L.control.geocoder(getSetting('_mapSearchKey'), {
+        focus: true,
+        position: getSetting('_mapSearch'),
+        zoom: trySetting('_mapSearchZoom', 12),
+        circle: true,
+        circleRadius: trySetting('_mapSearchCircleRadius', 1),
+        autocomplete: true,
+      }).addTo(map);
+    }
 
     // Add location control
     if (getSetting('_mapMyLocation') !== 'off') {
